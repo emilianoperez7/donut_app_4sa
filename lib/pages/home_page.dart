c8d8da9f56c531f1a423f67086f77c6269f22bdf
Emiliano Pérez <emilianojesusp599@gmail.com>
2025-02-13 13:33:16 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,11 +11,43 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          //Icono de la izquierda
-          leading: Icon(Icons.menu, color: Colors.grey[800]),
-          actions: [Icon(Icons.person)]),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            //Icono de la izquierda
+            leading: Icon(Icons.menu, color: Colors.grey[800]),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
+                child: Icon(Icons.person),
+              )
+            ]),
+        body: Column(
+          children: [
+            //1. Texto Principal (Text)
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
+              child: Row(
+                children: [
+                  Text("I want to "),
+                  Text("eat a donut",
+                      style: TextStyle(
+                        //Tamano de la fuente
+                        fontSize: 32,
+                        //Negritas
+                        fontWeight: FontWeight.bold,
+                        //Subrayado
+                        decoration: TextDecoration.underline,
+                      )),
+                ],
+              ),
+            ),
+            //2. Pestañas (TabBar)
+
+            //3. Contenido de pestañas (TabBarView)
+
+            //4. Carrito (Cart)
+          ],
+        ));
   }
 }
